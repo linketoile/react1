a3dd6f949e08b53a4010a624dbfc0ef2a7878828
linketoile <41509794+linketoile@users.noreply.github.com>
2022-05-16 14:10:19 +0200

    change with list item

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1>Test plan</h1>
-    <p>Test case</p>
+    <p>Test cases</p>
+    <ul>
+      <li> Tc1 </li>
+      <li> Tc2 </li>
+      <li> Tc3 </li>
+    </ul>
   </div>,
   document.getElementById("root")
 );

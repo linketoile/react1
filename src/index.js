a9dafd1a3cba70e2ff352774673f69d4485690a0
linketoile <41509794+linketoile@users.noreply.github.com>
2022-05-16 13:07:46 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Test plan</h1>
+    <p>Test case</p>
+  </div>,
+  document.getElementById("root")
+);

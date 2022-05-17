af4318b59b7cb2ac43d15a0586b60a488ec2e4e2
linketoile <41509794+linketoile@users.noreply.github.com>
2022-05-17 11:04:16 +0200

    Inline styling

--- src/index.js
+++ src/index.js
@@ -10,13 +10,38 @@ const year = currentDate.getFullYear();
 const img =
   "https://upload.wikimedia.org/wikipedia/commons/archive/5/53/20210618182605%21Google_%22G%22_Logo.svg";
 
+const customStyle = {
+  color: "black",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "darkblue";
+
 ReactDOM.render(
   <div>
     {/* class is className in js */}
     {/* html attributs change to camel case: */}
     <h1 className="heading" contentEditable="true" spellCheck="true">
-      Header 1
+      Title 1
     </h1>
+    <h2
+      style={{ color: "darkred" }}
+      className="heading"
+      contentEditable="true"
+      spellCheck="true"
+    >
+      Title 2
+    </h2>
+    <h3
+      style={customStyle}
+      className="heading"
+      contentEditable="true"
+      spellCheck="true"
+    >
+      Title 3
+    </h3>
+
     <p>Paragraph</p>
     <ul>
       <li> Tc1 </li>
